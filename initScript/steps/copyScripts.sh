7a533a1a3356aded7348ebd9310425421093b2cf
twoplus_studio <twoplus_studio@163.com>
2017-03-08 13:00:21 +0800

    add coments for test

--- initScript/steps/copyScripts.sh
+++ initScript/steps/copyScripts.sh
@@ -15,7 +15,7 @@ source /etc/profile
 # update /etc/crontab for 'clear_sys_mem_daily.sh'
 echo "
 
-# each 1 hrs run once time
+# each 1 hrs run once time, at HH:00 each hour
 0 * * * * root ${scripts_path}/wsworkenv/autoRun/clear_sys_mem_daily.sh
 
 # each 1 day run once time, at 00:01 (just 1 min later than normal daily rotation like log4j
