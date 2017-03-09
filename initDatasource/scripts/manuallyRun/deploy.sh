7ca3ad55031f96167b927fcb3d9a494956dff022
banlibo <banlibo@IBM007-PBF5M33.cn.ibm.com>
2017-03-09 17:06:46 +0800

    update deploy script

--- initDatasource/scripts/manuallyRun/deploy.sh
+++ initDatasource/scripts/manuallyRun/deploy.sh
@@ -21,9 +21,9 @@ ant -buildfile ${project_path}/${project_name}/build.xml
 
 # update jdbc connection parameter
 cd ${project_path}/${project_name}/WebContent/WEB-INF/classes/resource
-sed -i 's/jdbc.url=jdbc:mysql:\/\/127.0.0.1:xxx\/xxx/jdbc.url=jdbc:mysql:\/\/127.0.0.1:3308\/superwebsitebuilder/g' jdbc-mysql.properties
-sed -i 's/jdbc.username=xxx/jdbc.username=root/g' jdbc-mysql.properties
-sed -i 's/jdbc.password=xxx/jdbc.password=abcd@1234/g' jdbc-mysql.properties
+sed -i -e 's/^jdbc.url=/#&/g' -e '/jdbc.url=/a jdbc.url=jdbc:mysql:\/\/127.0.0.1:3308\/superwebsitebuilder' jdbc-mysql.properties
+sed -i -e 's/^jdbc.username=/#&/g' -e '/jdbc.username=/a jdbc.username=root' jdbc-mysql.properties
+sed -i -e 's/^jdbc.password=/#&/g' -e '/jdbc.password=/a jdbc.password=abcd@1234' jdbc-mysql.properties
 
 # make the change of web.xml to inform tomcat there is something changed
 webxml_file=${project_path}/${project_name}/WebContent/WEB-INF/web.xml
